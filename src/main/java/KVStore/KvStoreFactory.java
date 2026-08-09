fd964699e6d2d50f00eb6451f2ff07d398caf3d5
sahilbhandari <sbhandari791@gmail.com>
2026-08-10 02:27:49 +0530

    implementing shard kv crud operations

---
+++ src/main/java/KVStore/KvStoreFactory.java
@@ -0,0 +1,4 @@
+package KVStore;
+
+public class KvStoreFactory {
+}
---
+++ src/main/java/KVStore/strategy/InMemoryKVStore.java
@@ -0,0 +1,27 @@
+package KVStore;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryKVStore<K,V> implements KVStore<K,V>{
+
+    ConcurrentHashMap<K,V> map;
+
+    public InMemoryKVStore() {
+        map = new ConcurrentHashMap<>();
+    }
+    @Override
+    public V get(K key) {
+        return map.get(key);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        map.put(key, value);
+    }
+
+    @Override
+    public void delete(K key) {
+        if(map.containsKey(key))
+            map.remove(key);
+    }
+}
