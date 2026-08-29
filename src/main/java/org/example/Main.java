1b0089ae3b0c6e14ef9ec89d7bfc4f325339df65
sahilbhandari <sbhandari791@gmail.com>
2026-08-29 23:26:14 +0530

    remove unnecessary code snippet

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,33 +23,6 @@ import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
-//        String node = args[0];
-//        int port = Integer.parseInt(args[1]);
-//        String path = args[2];
-//
-//        ServerSocket serverSocket = new ServerSocket(port);
-
-//        while(true) {
-//            System.out.println("start recieveing data");
-//            Socket socket = serverSocket.accept();
-//            InputStream inputStream = socket.getInputStream();
-//
-//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-//            String request;
-//            while((request = bufferedReader.readLine()) != null) {
-//                System.out.println(request);
-//            }
-//            socket.close();
-//            //System.out.println(request);
-//        }
-
-
-//        String caller = args[0];
-//        if("A".equalsIgnoreCase(caller)) {
-//            serverA(args);
-//        } else if("B".equalsIgnoreCase(caller)) {
-//            serverB(args);
-//        }
         String nodeId = args[0];
         int port = Integer.parseInt(args[1]);
         String walName = args[2];
@@ -73,9 +46,6 @@ public class Main {
 
                 try {
                     while ((request = bufferedReader.readLine()) != null) {
-
-                        //System.out.println(response.getPayload());
-
                         try {
                             Response response = requestHandler.handleRequest(request);
                             bufferedWriter.write(response.getPayload() + "\n");
@@ -96,56 +66,5 @@ public class Main {
         NodeInfo nodeInfo1 = new NodeInfo("node-1",9091);
         NodeInfo nodeInfo2 = new NodeInfo("node-2",9092);
         return List.of(nodeInfo, nodeInfo1, nodeInfo2);
-    }
-    public static void serverA(String[] args) throws IOException {
-        String node = args[1];
-        int port = Integer.parseInt(args[2]);
-        String path = args[3];
-
-        ServerSocket serverSocket = new ServerSocket(port);
-
-        while(true) {
-            Socket socket = serverSocket.accept();
-            Socket targetSocket = new Socket("localhost",9091);
-            System.out.println("Request Router");
-            BufferedReader inputSocketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader targetSocketReader = new BufferedReader(new InputStreamReader(targetSocket.getInputStream()));
-            BufferedWriter inputSocketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            BufferedWriter targetSocketWriter = new BufferedWriter(new OutputStreamWriter(targetSocket.getOutputStream()));
-
-            String request = inputSocketReader.readLine();
-
-            System.out.println("client A recieved from nc: "+request);
-            targetSocketWriter.write(request+"\n");
-            targetSocketWriter.flush();
-
-
-            String response = targetSocketReader.readLine();
-            System.out.println("client A recieved from B: "+response);
-
-            inputSocketWriter.write("client A write to nc: "+response);
-            inputSocketWriter.flush();
-        }
-    }
-
-    public static void serverB(String[] args) throws IOException {
-        String node = args[1];
-        int port = Integer.parseInt(args[2]);
-        String path = args[3];
-
-        ServerSocket serverSocket = new ServerSocket(port);
-
-        while(true) {
-            Socket socket = serverSocket.accept();
-            BufferedWriter socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String input;
-            while((input = socketReader.readLine()) != null) {
-                System.out.println("client B recieved from A: "+input);
-                socketWriter.write("success\n");
-                socketWriter.flush();
-            }
-
-        }
     }
 }
