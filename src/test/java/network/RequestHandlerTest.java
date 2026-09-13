5df90884ccc2ebe56bb4380042c7e8558d7c1bed
sahilbhandari <sbhandari791@gmail.com>
2026-09-13 21:08:57 +0530

    request handler replication tests

--- src/test/java/network/RequestHandlerTest.java
+++ src/test/java/network/RequestHandlerTest.java
@@ -20,6 +20,8 @@ public class RequestHandlerTest {
     @TempDir
     Path tempDir;
     RequestHandler requestHandler;
+    RequestHandler requestHandler1;
+    RequestHandler requestHandler2;
 
     @BeforeEach
     public void setup() throws IOException {
@@ -27,9 +29,17 @@ public class RequestHandlerTest {
         NodeInfo nodeInfo1 = new NodeInfo("node-1", 9091);
         NodeInfo nodeInfo2 = new NodeInfo("node-2", 9092);
         ClusterConfiguration clusterConfiguration = new ClusterConfiguration("node-0", List.of(nodeInfo, nodeInfo1, nodeInfo2));
-        NodeManager nodeManager = new NodeManager(clusterConfiguration, tempDir.resolve("node-0.data"));
+        ClusterConfiguration clusterConfiguration1 = new ClusterConfiguration("node-1", List.of(nodeInfo, nodeInfo1, nodeInfo2));
+        ClusterConfiguration clusterConfiguration2 = new ClusterConfiguration("node-2", List.of(nodeInfo, nodeInfo1, nodeInfo2));
+        NodeManager nodeManager = new NodeManager(clusterConfiguration, tempDir.resolve("node-0-tmp.data"));
+        NodeManager nodeManager1 = new NodeManager(clusterConfiguration1, tempDir.resolve("node-1-tmp.data"));
+        NodeManager nodeManager2 = new NodeManager(clusterConfiguration1, tempDir.resolve("node-2-tmp.data"));
         ShardManager shardManager = new ShardManager(nodeManager, clusterConfiguration);
+        ShardManager shardManager1 = new ShardManager(nodeManager1, clusterConfiguration);
+        ShardManager shardManager2 = new ShardManager(nodeManager2, clusterConfiguration);
          requestHandler = new RequestHandler(shardManager, clusterConfiguration, nodeManager);
+         requestHandler1 = new RequestHandler(shardManager1, clusterConfiguration1, nodeManager1);
+         requestHandler2 = new RequestHandler(shardManager2, clusterConfiguration2, nodeManager2);
     }
 
     @Test
@@ -54,7 +64,7 @@ public class RequestHandlerTest {
 
     @Test
     public void shouldTestLocalNodeExecution() throws IOException {
-        Response actualResponse = requestHandler.handleRequest("PUT|user:0|Sahil_1");
+        Response actualResponse = requestHandler.handleRequest("PUT|user:0|test_case");
         Response expectedResponse = new Response(Response.Status.SUCCESS, "node-0", "operation successfull");
 
         assertEquals(expectedResponse, actualResponse);
@@ -63,5 +73,13 @@ public class RequestHandlerTest {
     @Test
     public void shouldThrowExceptionforRemoteCallFailure() throws IOException {
        assertThrows(ConnectException.class, () -> requestHandler.handleRequest("PUT|user:2|Sahil_1"));
+    }
+
+    @Test
+    public void shouldReplicateToReplicas() throws IOException {
+        Response actualResponsePUT = requestHandler.handleRequest("PUT|user:0|Sahil__1");
+        Response actualResponseGET = requestHandler.handleRequest("GET|user:0");
+        Response expectedResponseGET = new Response(Response.Status.VALUE, "node-0", "Sahil__1");
+        assertEquals(expectedResponseGET, actualResponseGET);
     }
 }
